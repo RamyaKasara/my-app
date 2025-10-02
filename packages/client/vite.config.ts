d2003a368ef679ad5d012c7ca51fbcb95c52b9fb
ramya.kasarabada@gmail.com <52570872+RamyaKasara@users.noreply.github.com>
2025-10-02 18:13:08 +0530

    Set up TailwindCSS

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   // proxy to direct all urls starting with /api to the server
   server: {
     proxy: {

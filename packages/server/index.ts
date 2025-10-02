2a9a88641731ab0a26b3e6a0fbae0bcd66906426
ramya.kasarabada@gmail.com <52570872+RamyaKasara@users.noreply.github.com>
2025-10-02 17:31:06 +0530

    Connect the frontend and backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,6 +1,6 @@
-import express from 'express';
-import type { Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import type { Request, Response } from "express";
+import dotenv from "dotenv";
 
 dotenv.config();
 //reads as stores all the vars in the .env file as environment variables in our application
@@ -13,11 +13,14 @@ const port = process.env.PORT || 3000;
 // and pass a function that will be executed when someone makes a request at this endpoint
 // the function takes two arguments : a request object and a response object
 // for now we will send a hello world message to the client
-app.get('/', (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
+});
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({ message: "Hello World!" });
 });
 
 //to start the webserver, takes a port and a callback function that will be executed once the server is up and running
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
